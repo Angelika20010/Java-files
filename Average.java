ed3f21a34610ac49e7b4c8f350222f55e2f1236f
Angelika20010 <111845568+Angelika20010@users.noreply.github.com>
2022-10-16 23:29:28 +0300

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,13 @@
+package dip107;
+
+
+import java.util.Scanner;
+
+public class Average {
+	   public static void main(String args[]) {
+		  
+		   for(int i=0; i<=10;i=i+2){
+		   System.out.println(i);
+		   }
+}
+}
